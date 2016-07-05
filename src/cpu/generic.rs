1c6f4fde24ef81d416391f1c48277fe61b6e0381
SASinestro <sasinestro@gmail.com>
2016-07-05 03:29:06 -0700

    This should do the right thing, but it doesn't.

---
+++ src/cpu/generic.rs
@@ -0,0 +1,6 @@
+// src/cpu/generic.rs
+// Copyright 2016 Alexis Williams
+//
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+
+// TODO: Write nice Register/Portmap abstraction
