01ad5e5feb7f89277ddce3bf3d5502a40761cd0c
SASinestro <sasinestro@gmail.com>
2016-07-06 19:23:21 -0700

    There's going to be more than one generic thing.

--- src/cpu/generic/mod.rs
+++ src/cpu/generic/mod.rs
@@ -1,6 +1,5 @@
-// src/cpu/generic.rs
+// src/cpu/generic/mod.rs
 // Copyright 2016 Alexis Williams
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 
-// TODO: Write nice Register/Portmap abstraction
